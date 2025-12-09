ca3a02daaca1949a7df6a5a3b05b27a0aa457725
Ibrahim K. Khalil <imkhalil@2p.com.sa>
2025-12-09 13:49:46 +0200

    Product

---
+++ src/Services/Catalog/Catelog.API/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Catelog.API.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public List<string> Category { get; set; } = new();
+        public string Description { get; set; }
+        public string ImgaeFile { get; set; }
+        public decimal Price { get; set; }
+    }
+}
