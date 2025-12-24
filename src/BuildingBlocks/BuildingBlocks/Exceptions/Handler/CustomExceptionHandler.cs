f158b31de89db4e088a937667a106f55a9678d85
Ibrahim K. Khalil <imkhalil@2p.com.sa>
2025-12-24 13:22:46 +0200

    Global Exception Handling with IExceptionHandler interface in Asp.net Core

---
+++ src/BuildingBlocks/BuildingBlocks/Exceptions/Handler/CustomExceptionHandler.cs
@@ -0,0 +1,72 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Reflection;
+
+namespace BuildingBlocks.Exceptions.Handler
+{
+    public class CustomExceptionHandler(ILogger<CustomExceptionHandler> logger) : IExceptionHandler
+    {
+        public async ValueTask<bool> TryHandleAsync(HttpContext context, Exception exception, CancellationToken cancellationToken)
+        {
+            logger.LogError(
+                    "Error Message: {exceptionMessage}, Time or occurrence {time}",
+                    exception, DateTime.UtcNow
+                );
+
+            (string Detail, string Title, int StatusCode) details = exception switch
+            {
+                InternalServerException =>
+                (
+                    exception.Message,
+                    exception.GetType().Name,
+                    context.Response.StatusCode = StatusCodes.Status500InternalServerError
+                ),
+                ValidationException =>
+                (
+                    exception.Message,
+                    exception.GetType().Name,
+                    context.Response.StatusCode = StatusCodes.Status400BadRequest
+                ),
+                BadRequestException =>
+                (
+                    exception.Message,
+                    exception.GetType().Name,
+                    context.Response.StatusCode = StatusCodes.Status400BadRequest
+                ),
+                NotFoundException =>
+                (
+                    exception.Message,
+                    exception.GetType().Name,
+                    context.Response.StatusCode = StatusCodes.Status404NotFound
+                ),
+
+                _ =>
+                (
+                    exception.Message,
+                    exception.GetType().Name,
+                    context.Response.StatusCode = StatusCodes.Status500InternalServerError
+                ),
+            };
+
+            var problemDetails = new ProblemDetails
+            {
+                Title = details.Title,
+                Detail = details.Detail,
+                Status = details.StatusCode,
+                Instance = context.Request.Path
+
+            };
+            problemDetails.Extensions.Add("traceId", context.TraceIdentifier);
+
+            if (exception is ValidationException validationEx) {
+                problemDetails.Extensions.Add("ValidationErrors", validationEx.Errors);
+            }
+            await context.Response.WriteAsJsonAsync( problemDetails );
+
+            return true;
+        }
+    }
+}
