e6f8d1436f343795cf0d4d3c7e5aac6b597a46a0
Ibrahim K. Khalil <imkhalil@2p.com.sa>
2025-12-24 16:04:51 +0200

    LoggingBehavior not implemented yet

---
+++ src/BuildingBlocks/BuildingBlocks/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingBlocks.Behaviors
+{
+    public class LoggingBehavior<TRequest, TResponse>(ILogger<LoggingBehavior> logger)
+        : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : notnull, IRequest<TRequest>
+        where TResponse : notnull
+    {
+        public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+
+        }
+    }
+}
