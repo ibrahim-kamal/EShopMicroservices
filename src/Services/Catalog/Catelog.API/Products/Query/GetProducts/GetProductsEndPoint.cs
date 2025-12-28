7bd34d9995af650264abd603d91d7e04131232ad
Ibrahim K. Khalil <imkhalil@2p.com.sa>
2025-12-28 11:23:37 +0200

    Pagination

--- src/Services/Catalog/Catelog.API/Products/Query/GetProducts/GetProductsEndPoint.cs
+++ src/Services/Catalog/Catelog.API/Products/Query/GetProducts/GetProductsEndPoint.cs
@@ -2,21 +2,21 @@
 {
     public class GetProductsEndPoint : CarterModule
     {
-        public record GetProductsRequest();
+        public record GetProductsRequest(int? PageNumber=1,int? PageSize=10);
         public record GetProductsResponse(IEnumerable<Product> Products);
         public override void AddRoutes(IEndpointRouteBuilder app)
         {
-            app.MapGet("/products", async (ISender sender) =>
+            app.MapGet("/products", async ([AsParameters] GetProductsRequest request, ISender sender) =>
             {
-                var result = await sender.Send(new GetProductsQuery());
+                var result = await sender.Send(request.Adapt<GetProductsQuery>());
                 var response = result.Adapt<GetProductsResponse>();
                 return Results.Ok(response);
             })
-                .WithName("GetProducts")
-                .Produces<GetProductsResponse>(StatusCodes.Status200OK)
-                .ProducesProblem(StatusCodes.Status400BadRequest)
-                .WithDescription("Get Products")
-                .WithSummary("Get Products");
+            .WithName("GetProducts")
+            .Produces<GetProductsResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithDescription("Get Products")
+            .WithSummary("Get Products");
         }
     }
 }
--- src/Services/Catalog/Catelog.API/Products/Query/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/Catelog.API/Products/Query/GetProducts/GetProductsHandler.cs
@@ -1,7 +1,8 @@
-﻿namespace Catelog.API.Products.Query.GetProducts
+﻿
+namespace Catelog.API.Products.Query.GetProducts
 {
 
-    public record GetProductsQuery() : IQuery<GetProductsResult>;
+    public record GetProductsQuery(int? PageNumber = 1, int? PageSize = 10) : IQuery<GetProductsResult>;
     public record GetProductsResult(IEnumerable<Product> Products);
     public class GetProductsHandler(IDocumentSession documentSession): IQueryHandler<GetProductsQuery, GetProductsResult>
     {
@@ -9,7 +10,7 @@
         public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
         {
             var products = await documentSession.Query<Product>()
-                    .ToListAsync(cancellationToken);
+                    .ToPagedListAsync(query.PageNumber ?? 1, query.PageSize ?? 10,cancellationToken);
             return new GetProductsResult(products);
         }
     }
