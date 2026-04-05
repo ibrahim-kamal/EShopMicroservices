e60fed7ae05e17b7858c9233a333efc97c5c6d18
ibrahim kamal <ibrahim.kamal2997@gmail.com>
2026-04-05 23:06:20 +0200

    Add MassTransit with RabbitMQ and refactor broker setup
    Refactored AddMessageBroker to extend IServiceCollection and accept an optional Assembly for consumer registration. Configured MassTransit with RabbitMQ using settings from configuration. Added necessary NuGet packages and updated using directives.

--- src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extentions.cs
+++ src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extentions.cs
@@ -1,13 +1,38 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿
+using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace BuildingBlocks.Messaging.MassTransit
 {
     public static class Extentions
     {
-        public static IServiceProvider AddMessageBroker(this IServiceProvider service,IConfiguration configuration, AssemblyLoadEventArgs? assembly =null) { 
-            
+        public static IServiceProvider AddMessageBroker
+            (this IServiceCollection services,IConfiguration configuration, Assembly? assembly =null) 
+        {
+
             //Implement RabbitMq MassTransit configuration
-            return service;
+            services.AddMassTransit(config =>
+            {
+                config.SetKebabCaseEndpointNameFormatter();
+                if (assembly != null)
+                    config.AddConsumers(assembly);
+
+                config.UsingRabbitMq((context, Configurator) =>
+                {
+                    Configurator.Host(new Uri(configuration["MessageBroker:Host"]!), host =>
+                    {
+                    });
+                });
+
+
+            });
+
+            
+
+
+            return services;
         }
     }
 }
