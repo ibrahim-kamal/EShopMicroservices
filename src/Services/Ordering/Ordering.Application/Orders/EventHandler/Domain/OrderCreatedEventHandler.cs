7fe9e78ad2beea5eca2f93567e85043cc9bd72b9
Ibrahim K. Khalil <imkhalil@2p.com.sa>
2026-04-09 15:28:22 +0200

    Integrate RabbitMQ and MassTransit for event publishing
    Updated OrderCreatedEventHandler to publish integration events via MassTransit using IPublishEndpoint. Added RabbitMQ message broker service to docker-compose files to enable message-based communication between services.

--- src/Services/Ordering/Ordering.Application/Orders/EventHandler/Domain/OrderCreatedEventHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandler/Domain/OrderCreatedEventHandler.cs
@@ -1,12 +1,16 @@
-﻿namespace Ordering.Application.Orders.EventHandler.Domain
-{
-    public class OrderCreatedEventHandler(ILogger<OrderCreatedEventHandler> logger) : INotificationHandler<OrderCreatedEvent>
-    {
-        public Task Handle(OrderCreatedEvent notification, CancellationToken cancellationToken)
-        {
-            logger.LogInformation("Domain Event handled: {DomainEvent}", notification.GetType().Name);
+﻿using MassTransit;
 
-            return Task.CompletedTask;
+namespace Ordering.Application.Orders.EventHandler.Domain
+{
+    public class OrderCreatedEventHandler(IPublishEndpoint publishEndpoint,ILogger<OrderCreatedEventHandler> logger) : INotificationHandler<OrderCreatedEvent>
+    {
+        public async Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Domain Event handled: {DomainEvent}", domainEvent.GetType().Name);
+
+            var orderCreateIntegrationEvent = domainEvent.order.ToOrderDto();
+
+            await publishEndpoint.Publish(orderCreateIntegrationEvent,cancellationToken);
         }
     }
 }
