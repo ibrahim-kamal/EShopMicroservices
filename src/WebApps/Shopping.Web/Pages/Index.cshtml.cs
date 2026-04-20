f28b78ef8255414fb292beb031adab7c65a9ffcf
Ibrahim K. Khalil <imkhalil@2p.com.sa>
2026-04-20 16:12:54 +0200

    Add basket support and 'Add to cart' handler to Index page
    Refactored IndexModel to inject IBasketService and renamed OnGet to OnGetAsync. Added OnPostAddToCartAsync handler to process add-to-cart actions, including logging, product retrieval, and loading the user's basket. Introduced LoadUserBasket helper to fetch or initialize the shopping cart.

--- src/WebApps/Shopping.Web/Pages/Index.cshtml.cs
+++ src/WebApps/Shopping.Web/Pages/Index.cshtml.cs
@@ -1,17 +1,52 @@
+
+
 namespace Shopping.Web.Pages
 {
-    public class IndexModel(ICatalogService catalogServices,ILogger<IndexModel> logger) : PageModel
+    public class IndexModel(ICatalogService catalogServices, IBasketService basketService, ILogger<IndexModel> logger) : PageModel
     {
 
         public IEnumerable<ProductModel> ProductList { get; set; } = new List<ProductModel>();
 
 
-        public async Task<IActionResult> OnGet()
+        public async Task<IActionResult> OnGetAsync()
         {
             logger.LogInformation("Index page visited");
             var result = await catalogServices.GetProducts();
             ProductList = result.Products;
             return Page();
+        }
+
+
+        public async Task<IActionResult> OnPostAddToCartAsync(Guid productId)
+        {
+            logger.LogInformation("Add to cart button clicked productId:{productId}", productId);
+            var productResponse = await catalogServices.GetProduct(productId);
+            var basket = await LoadUserBasket();
+
+            //return Ok();
+        }
+
+        private async Task<ShoppingCartModel> LoadUserBasket()
+        {
+            var username = "dev";
+            ShoppingCartModel basket;
+            try
+            {
+                var getBasketResponse = await basketService.GetBasket(username);
+                basket = getBasketResponse.Cart;
+
+            }
+            catch (Exception ex)
+            {
+
+                basket = new ShoppingCartModel()
+                {
+                    UserName = username,
+                    Items = []
+                };
+            }
+
+            return basket;
         }
     }
 }
